908dd415819be6a09d9996edd8129da82065b201
Bethany Hitch <bethany.e.hitch@gmail.com>
2022-05-25 10:26:31 -0700

    Update puzzle words

---
+++ update-wordlist.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+const fetchJS = async () => {
+  const urlBase = 
+    'https://www.nytimes.com/games/wordle/'
+  const url = urlBase + 'index.html'
+  
+  try {
+    const html = await (await fetch(url)).text()
+    
+    const scriptName =
+      html.match(/\<script src="(main\.[^"]+)"/)[1]
+    const script = 
+      await (await fetch(urlBase + scriptName)).text()
+      
+    const puzzleList = JSON.parse(
+      script.match(/\[\s*["']CIGAR[^\]]+\]/i)[0]
+        .toUpperCase()
+    )
+      
+    return puzzleList.join('\n')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+fetchJS().then(list =>
+  fs.writeFileSync(
+    './data/puzzle-words.txt'
+  )
+)
